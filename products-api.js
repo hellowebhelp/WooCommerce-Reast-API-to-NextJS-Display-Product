c38d16c7ef243d7294ea006c0fbe29966d65f12f
Hellowebhelp.com <32351854+hellowebhelp@users.noreply.github.com>
2024-01-03 16:03:50 +0600

    Create products-api.js
    Facing woo api data

---
+++ products-api.js
@@ -0,0 +1,38 @@
+// https://www.npmjs.com/package/@woocommerce/woocommerce-rest-api
+// npm i @woocommerce/woocommerce-rest-api
+
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+const api = new WooCommerceRestApi({
+  
+    url: "https://example.com",
+    consumerKey: "ck_d48249a2e51adcd6ce8544915212f0efccf4f988",
+    consumerSecret: "cs_493b3b4137bb308f74e5ca295ec860407654c211",
+    version: "wc/v3"
+});
+
+export default async function handler(req, res) {
+    const responseData = {
+        success: false,
+    }
+
+    try {
+        const { perpage } = req?.query ?? {};
+        const { data } = await api.get(
+            'products',
+            {
+                //per_page: perpage || 504, //example: ?perpage=1
+                per_page: 199,
+                status: 'publish',
+
+            }
+        );
+
+        responseData.success = true;
+        res.json(data);
+
+    } catch (error) {
+        responseData.error = error.message;
+        res.status(500).json(responseData);
+    }
+}
+
