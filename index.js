58792bc899c8da203ee1c10c52e5db1012d7acae
Hellowebhelp.com <32351854+hellowebhelp@users.noreply.github.com>
2024-01-03 16:16:24 +0600

    Create index.js
    Display woocommerce product using NextJs

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * All products collection.
+ * 
+ * Development by Hellowebhelp.com
+ */
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+
+export const getStaticProps = async () => {
+
+    // For Products Fetch
+    const res = await fetch(`https://example.com/api/products-api`);
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const ProductData = await res.json();
+
+    return {
+        props: { ProductData }, // Products array
+    };
+}
+
+
+const All_collection = ({ ProductData, }) => {
+    return (
+        <>
+            <section>
+                <div className="custom_container">
+                    {ProductData?.map(
+                        (Display) => (
+                            <Link href={'#'} key={Display.id}>
+                                <div>{Display.name}</div>
+                            </Link>
+                        )
+                    )}
+                </div>
+            </section>
+        </>
+    );
+}
+export default All_collection;
